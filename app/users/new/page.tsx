5e31d75adf54b2da4ccc24cda05338e7171f02c0
Utkarsh Raj Sinha <ur5441@srmist.edu.in>
2025-03-13 00:49:45 +0530

    nextjsdemo files

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NewUserPage = () => {
-  return (
-    <div>NewUserPage</div>
-  )
-}
+  return <div>NewUserPage</div>;
+};
 
-export default NewUserPage
+export default NewUserPage;
